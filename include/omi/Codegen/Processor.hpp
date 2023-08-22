7a240bd480243fbaa11c2f1b9cecde174f9f663c
CanftIn <wwc7033@gmail.com>
2023-08-22 14:27:23 +0800

    add codegen/jit/sema processor

---
+++ include/omi/Codegen/Processor.hpp
@@ -0,0 +1,16 @@
+#ifndef OMI_CODEGEN_PROCESSOR_HPP
+#define OMI_CODEGEN_PROCESSOR_HPP
+
+namespace omi::runtime {
+struct context;
+}  // namespace omi::runtime
+
+namespace omi::codegen {
+
+struct processor {
+  processor() = delete;
+};
+
+}  // namespace omi::codegen
+
+#endif  // OMI_CODEGEN_PROCESSOR_HPP
---
+++ include/omi/Jit/Processor.hpp
@@ -0,0 +1,14 @@
+#ifndef OMI_JIT_PROCESSOR_HPP
+#define OMI_JIT_PROCESSOR_HPP
+
+#include "omi/Type/Result.hpp"
+
+namespace omi::jit {
+
+struct processor {
+  processor() = default;
+};
+
+}  // namespace omi::jit
+
+#endif  // OMI_JIT_PROCESSOR_HPP
